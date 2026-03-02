ee30dfb5d34f7f4fee7a4b312830c575ae9ab7d4
Shanosha <wootenm1@gmail.com>
2026-03-02 15:18:33 -0500

    feature/setup favorites context

--- src/AppProviders.jsx
+++ src/AppProviders.jsx
@@ -1,12 +1,17 @@
 import { createContext } from "react";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 export const FavoritesContext = createContext(null);
 
 export function AppProviders({children}) {
+    const [favorites,setFavorites] = useLocalStorage("favoriteRecipes")
 
+    const addFavorite = (id) => setFavorites([...favorites,id])
+    const removeFavorite = (id) => setFavorites(favorites.filter((fav)=>fav!==id))
+    const isFavorite = (id) => favorites.find(id)
     
     return (
-        <FavoritesContext.Provider>
+        <FavoritesContext.Provider value={{favorites,addFavorite,removeFavorite,isFavorite}}>
             {children}
         </FavoritesContext.Provider>
     )
