7c0aacdf0fccaf2c5a005e2666d69cec4bee1cc9
Shanosha <wootenm1@gmail.com>
2026-03-04 13:03:29 -0500

    feature/added favorite recipe listings to favorites page

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,7 +1,26 @@
+import { RecipeCard } from "../components/RecipeCard";
+import { useContext } from "react";
+import { FavoritesContext } from "../context/FavoritesContext";
+
 function FavoritesPage() {
+
+    const {favorites} = useContext(FavoritesContext)
+    console.log("favorites: ",favorites)
     return (
         <>
-            <h1>FavoritesPage</h1>
+            <h2>Favorite Recipes</h2>
+
+            {favorites.length>0 ?
+                <ul>
+                    {favorites.map(element => 
+                        <RecipeCard key={element.idMeal} meal={element} />
+                    )}
+                </ul>
+                :
+                <p>
+                You don't currently have any meals saved to your favorites list.
+                </p>
+            }
         </>
     )
 }
